364098721fb5870bf299274e66aa02dfaf84ea59
Ricardo Dias <65636696+RD-Ricardo@users.noreply.github.com>
2021-05-11 12:16:27 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,12 +6,12 @@ namespace bubleSorte
     {
         static void Main(string[] args)
         {
-
+            
             Console.WriteLine("Digite 1 para gerar");
             int[] num = new int[10];
             int i, aux, fim;
             Random r = new Random();
-
+            //1 passo
             for(i = 0 ; i <=9 ; i++){
                 num [i] = r.Next(50);
                 Console.WriteLine(num[i]);
